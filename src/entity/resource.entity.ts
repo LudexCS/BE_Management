b529236c22f3eb129ef43f36769ba40aaedee6d7
Gomdadi <35wnsgh@naver.com>
2025-05-07 14:46:41 +0900

    feat: resource entity 수정

--- src/entity/resource.entity.ts
+++ src/entity/resource.entity.ts
@@ -1,50 +1,45 @@
-import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    CreateDateColumn,
-    UpdateDateColumn
-} from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity('resource')
 export class Resource {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({name: 'game_id', type: 'int', nullable: true, default: 0})
+    @Column({ name: 'game_id', type: 'int', nullable: true, default: () => '0' })
     gameId: number;
 
-    @Column({name: 'user_id', type: 'int', nullable: true, default: 0})
+    @Column({ name: 'user_id', type: 'int', nullable: true, default: () => '0' })
     userId: number;
 
-    @Column({name: 'usage_type_id', type: 'int'})
-    usageTypeId: number;
+    @Column({ name: 'seller_ratio', type: 'tinyint', unsigned: true })
+    sellerRatio: number;
 
-    @Column({name: 'allow_derivation', type: 'boolean'})
+    @Column({ name: 'creator_ratio', type: 'tinyint', unsigned: true })
+    creatorRatio: number;
+
+    @Column({ name: 'allow_derivation', type: 'boolean' })
     allowDerivation: boolean;
 
-    @Column({name: 'additional_condition', type: 'text', nullable: true})
+    @Column({ name: 'additional_condition', type: 'text', nullable: true })
     additionalCondition: string;
 
-    @Column({type: 'text', nullable: true})
+    @Column({ type: 'text', nullable: true })
     description: string;
 
-    @Column({name: 'download_times', type: 'int', default: 0})
+    @Column({ name: 'download_times', type: 'int', default: () => '0' })
     downloadTimes: number;
 
     @CreateDateColumn({
         type: 'timestamp',
         name: 'registered_at',
-        nullable: true,
-        default: () => 'CURRENT_TIMESTAMP'
+        default: () => 'CURRENT_TIMESTAMP',
     })
     registeredAt: Date;
 
     @UpdateDateColumn({
         type: 'timestamp',
         name: 'updated_at',
-        nullable: true,
-        default: () => 'CURRENT_TIMESTAMP'
+        default: () => 'CURRENT_TIMESTAMP',
     })
     updatedAt: Date;
 }
