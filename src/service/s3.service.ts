ae2b18ffd075a3222e91b98bacb424dd219ca109
gart09 <69684927+gart09@users.noreply.github.com>
2025-05-26 02:37:05 +0900

    feat: 이미지업로드시 기존이미지 삭제

--- src/service/s3.service.ts
+++ src/service/s3.service.ts
@@ -1,5 +1,5 @@
 import { S3 } from "../config/s3.config";
-import {PutObjectCommand} from "@aws-sdk/client-s3";
+import {PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import {v4 as uuidv4} from 'uuid';
 import { readFile } from 'fs/promises';
 
@@ -37,4 +37,11 @@ export const uploadResourceImageToS3 = async ( image: { path: string; mimetype: 
     const url = `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
 
     return { url, key };
+};
+
+export const deleteFromS3 = async (key: string) => {
+    await S3.send(new DeleteObjectCommand({
+        Bucket: process.env.AWS_S3_BUCKET,
+        Key: key
+    }));
 };
