330b60f7b4f324d4eb35ca75b38a9575b366a469
gart09 <gart09@naver.com>
2025-05-30 21:37:18 +0900

    fix: 롤백 및 차단게임 안뜨게

--- src/controller/showGameList.controller.ts
+++ src/controller/showGameList.controller.ts
@@ -1,14 +1,14 @@
 import { getGameList, getOriginGameInfo, getVariantGameInfo } from '../service/showGameList.service'
 import { GameListRequestDto} from "../dto/gameListRequest.dto";
 
-export const loadGameListControl = async (gameListRequestDto: GameListRequestDto, isAdmin: boolean) =>{
-    return await getGameList(gameListRequestDto, isAdmin);
+export const loadGameListControl = async (gameListRequestDto: GameListRequestDto) =>{
+    return await getGameList(gameListRequestDto);
 }
 
-export const showOriginGameHierarchyControl = async (gameId: number, isAdmin: boolean) =>{
-    return await getOriginGameInfo(gameId, isAdmin);
+export const showOriginGameHierarchyControl = async (gameId: number) =>{
+    return await getOriginGameInfo(gameId);
 }
 
-export const showVarientGameHierarchyControl = async (gameId: number, isAdmin: boolean) =>{
-    return await getVariantGameInfo(gameId, isAdmin);
+export const showVarientGameHierarchyControl = async (gameId: number) =>{
+    return await getVariantGameInfo(gameId);
 }
